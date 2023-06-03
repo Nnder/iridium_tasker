6c4ed87795c91ef6e32697022b5c492debd0827f
Nnder <asfahaikrkt6k56@yandex.ru>
2023-06-03 11:33:50 +0500

    force

--- web/public/token.php
+++ web/public/token.php
@@ -1,4 +1,5 @@
 <?php
 //$token = '6284064559:AAHxBH9ohl-gXbyfhxwGm0I5Td9m-KOsHJo'//5712309589:AAFyq_C9l39rDgsgApuy80Cfb0dlshyFZO8'
-$token = $_ENV['BOT_TOKEN']
+$token = '5724300852:AAH-mzNNCevq2xTe6cETkajOTdrD__wPvew';
+//$token = $_ENV['BOT_TOKEN']
 ?>
