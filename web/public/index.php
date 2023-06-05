4b91efd979e47685f148735a7e7d3bad548520a3
Vidombax <129399292+Vidombax@users.noreply.github.com>
2023-06-05 10:46:49 +0500

    latest web-interface

--- web/public/index.php
+++ web/public/index.php
@@ -287,6 +287,9 @@ if (access($connection) == 4) {
 </div>
 <?php
     }
+    else {
+      
+    }
   }
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js?v=<?= time(); ?>"></script>
