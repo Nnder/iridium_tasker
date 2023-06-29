dc661b564d340355470ddffcf113064547c1fe76
Nnder <asfahaikrkt6k56@yandex.ru>
2023-06-29 20:36:37 +0500

    merge

--- bot/proj/ProjectConfig.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    webAppUrl : 'https://webapp.test.iridi.com/',
-    token : '5724300852:AAH-mzNNCevq2xTe6cETkajOTdrD__wPvew' //'5712309589:AAFyq_C9l39rDgsgApuy80Cfb0dlshyFZO8'
-}
